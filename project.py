efc76c4b7b16553e77e35812262a14f3b4d7bf99
Evan Thomas <ethomas93@gatech.edu>
2025-03-26 16:46:59 -0400

    initial function names

---
+++ project.py
@@ -0,0 +1,16 @@
+def generate_initial_state():
+
+def euler_step():
+
+def runge_kutta_step():
+
+#def crank_nicholson_step():
+
+def dpsi_dt_undamped():
+
+def dpsi_dt_damped():
+
+def time_dependent_solution():
+
+def eigenstate_solution():
+
