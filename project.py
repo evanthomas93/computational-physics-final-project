4fc1efe10f61181058249445ec216f43f9e8f30f
Evan Thomas <ethomas93@gatech.edu>
2025-03-28 15:24:40 -0400

    finished eigenstate solution

--- project.py
+++ project.py
@@ -10,9 +10,11 @@ C_n = [1/np.sqrt(3), 1/np.sqrt(3), 1/np.sqrt(3)] # initial values of first three
 average_n = (np.dot(C_n,np.arange(1,4)))/3.0 # average energy level, used to get classical turning point 
 N = 201
 M = 100
+T = 10*2*np.pi/omega # run for 10 times the period of the harmonic oscillator
 x_classical_turning_point = np.sqrt((2*average_n+1)*(hbar/(m*omega)))
 x_max = x_classical_turning_point*5.0
 x_vec = np.linspace(-x_max, x_max, N)
+t_vec = np.linspace(0,T,M)
 h =(2*x_max)/(N-1)
 
 def generate_initial_state():
@@ -50,13 +52,15 @@ def dpsi_dt_damped(psi, x, t, alpha):
     return dpsidt
 
 #def time_dependent_solution():
-
+def E_n(n):
+    return (n+0.5)*hbar*omega
 def eigenstate_solution():
-    psi = np.zeros((N,M))
-    for i in range(N):
-        psi[:,0] += C_n[0]*(m*omega/np.pi/hbar)**0.25*np.exp(-m*omega*x_vec**2.0/(2*hbar))
-        psi[:,0] += C_n[1]*(4/np.pi*(m*omega/hbar)**3)**0.25*x_vec*np.exp(-m*omega*x_vec**2.0/(2*hbar))
-        psi[:,0] += C_n[2]*(m*omega/4/np.pi/hbar)**0.25*(2*m*omega/hbar*x_vec**2.0-1)*np.exp(-m*omega*x_vec**2.0/2/hbar)
+    psi = np.zeros((N,M),np.complex128)
+    for i in range(M):
+        t = t_vec[i]
+        psi[:,i] += np.exp(-1.0j*E_n(0)*t/hbar)*C_n[0]*(m*omega/np.pi/hbar)**0.25*np.exp(-m*omega*x_vec**2.0/(2*hbar))
+        psi[:,i] += np.exp(-1.0j*E_n(1)*t/hbar)*C_n[1]*(4/np.pi*(m*omega/hbar)**3)**0.25*x_vec*np.exp(-m*omega*x_vec**2.0/(2*hbar))
+        psi[:,i] += np.exp(-1.0j*E_n(2)*t/hbar)*C_n[2]*(m*omega/4/np.pi/hbar)**0.25*(2*m*omega/hbar*x_vec**2.0-1)*np.exp(-m*omega*x_vec**2.0/2/hbar)
     return psi # N x M for N points, M timesteps
 
 # solve time dependent equation for all three timestepping methods
