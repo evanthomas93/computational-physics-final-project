e83b52f362241091b7b6efb2ee097a4a0aaabe2c
Evan Thomas <ethomas93@gatech.edu>
2025-03-31 16:15:40 -0400

    first attempt at Euler time step

--- project.py
+++ project.py
@@ -5,27 +5,27 @@ import matplotlib.pyplot as plt
 m = 1
 hBar = 1.055e-34 # Reduced Planck's Constant in J * s
 omega = 1
-hbar = 1
 C_n = [1/np.sqrt(3), 1/np.sqrt(3), 1/np.sqrt(3)] # initial values of first three eigenstates
 average_n = (np.dot(C_n,np.arange(1,4)))/3.0 # average energy level, used to get classical turning point 
-N = 201
-M = 100
-T = 10*2*np.pi/omega # run for 10 times the period of the harmonic oscillator
-x_classical_turning_point = np.sqrt((2*average_n+1)*(hbar/(m*omega)))
+N = 401
+M = 2000
+T = 1*2*np.pi/omega # run for 10 times the period of the harmonic oscillator
+x_classical_turning_point = np.sqrt((2*average_n+1)*(hBar/(m*omega)))
 x_max = x_classical_turning_point*5.0
 x_vec = np.linspace(-x_max, x_max, N)
 t_vec = np.linspace(0,T,M)
+dt = t_vec[1]-t_vec[0]
 h =(2*x_max)/(N-1)
 
 def generate_initial_state():
     # get initial psi
     return psi
 
-def euler_step(psi, dpsi_dt_function, dt):
+def euler_step(psi, dpsi_dt_function, t, dt):
+    return psi + dpsi_dt_function(psi, x_vec, t)*dt
+def runge_kutta_step(psi, dpsi_dt_function, t, dt):
     pass
-def runge_kutta_step(psi, dpsi_dt_function, dt):
-    pass
-def crank_nicholson_step(psi, dpsi_dt_function, dt):
+def crank_nicholson_step(psi, dpsi_dt_function, t, dt):
     pass
     # might need a separate version for damped and undamped
 
@@ -53,15 +53,31 @@ def dpsi_dt_damped(psi, x, t, alpha):
 
 #def time_dependent_solution():
 def E_n(n):
-    return (n+0.5)*hbar*omega
+    return (n+0.5)*hBar*omega
 def eigenstate_solution():
     psi = np.zeros((N,M),np.complex128)
     for i in range(M):
         t = t_vec[i]
-        psi[:,i] += np.exp(-1.0j*E_n(0)*t/hbar)*C_n[0]*(m*omega/np.pi/hbar)**0.25*np.exp(-m*omega*x_vec**2.0/(2*hbar))
-        psi[:,i] += np.exp(-1.0j*E_n(1)*t/hbar)*C_n[1]*(4/np.pi*(m*omega/hbar)**3)**0.25*x_vec*np.exp(-m*omega*x_vec**2.0/(2*hbar))
-        psi[:,i] += np.exp(-1.0j*E_n(2)*t/hbar)*C_n[2]*(m*omega/4/np.pi/hbar)**0.25*(2*m*omega/hbar*x_vec**2.0-1)*np.exp(-m*omega*x_vec**2.0/2/hbar)
+        psi[:,i] += np.exp(-1.0j*E_n(0)*t/hBar)*C_n[0]*(m*omega/np.pi/hBar)**0.25*np.exp(-m*omega*x_vec**2.0/(2*hBar))
+        psi[:,i] += np.exp(-1.0j*E_n(1)*t/hBar)*C_n[1]*(4/np.pi*(m*omega/hBar)**3)**0.25*x_vec*np.exp(-m*omega*x_vec**2.0/(2*hBar))
+        psi[:,i] += np.exp(-1.0j*E_n(2)*t/hBar)*C_n[2]*(m*omega/4/np.pi/hBar)**0.25*(2*m*omega/hBar*x_vec**2.0-1)*np.exp(-m*omega*x_vec**2.0/2/hBar)
     return psi # N x M for N points, M timesteps
+
+psi_eigenstates = eigenstate_solution()
+
+psi_undamped_euler = np.zeros_like(psi_eigenstates, np.complex128)
+psi_undamped_euler[:,0] = psi_eigenstates[:,0]
+for i in range(M):
+    print(i)
+    if i > 0:
+        psi_undamped_euler[0,i-1] = 0.0
+        psi_undamped_euler[-1,i-1] = 0.0
+        psi_undamped_euler[:,i] = euler_step(psi_undamped_euler[:,i-1], dpsi_dt_undamped, t_vec[i], dt)
+    #plt.plot(np.real(psi_undamped_euler[:,i]))
+    #plt.plot(np.imag(psi_undamped_euler[:,i]))
+    #plt.show(block=False)
+    #plt.savefig("euler/{}.png".format(i))
+    #plt.clf()
 
 # solve time dependent equation for all three timestepping methods
 # compare to eigenstate solution
